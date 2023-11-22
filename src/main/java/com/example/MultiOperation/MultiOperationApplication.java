fa8015966a983a6534201ead5ab1ceb272e199ea
Naini-12 <93852211+Naini-12@users.noreply.github.com>
2023-11-22 22:47:12 +0530

    new

---
+++ src/main/java/com/example/MultiOperation/MultiOperationApplication.java
@@ -0,0 +1,13 @@
+package com.example.MultiOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiOperationApplication.class, args);
+	}
+
+}
